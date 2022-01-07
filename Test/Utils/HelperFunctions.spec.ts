52e01eac86fcf6c8fbdc00cc55b85a6c075094a2
Steven Pollack <steven.pollack@proxima.capital>
2022-01-08 10:18:58 +1100

    test: switch to qunit UI but keep failing suite

--- Test/Utils/HelperFunctions.spec.ts
+++ Test/Utils/HelperFunctions.spec.ts
@@ -2,54 +2,52 @@ import { GetAllTestFiles, GetMatchingFiles, MapArgs } from "@Src/Utils/HelperFun
 import { TEST_DIR } from "@Src/Utils/Paths";
 import { expect } from "chai";
 
-describe("GetAllTestFiles", () =>
-{
-    it("correctly filters specified files", () =>
-    {
-        const lActual: string[] = GetAllTestFiles(TEST_DIR);
-        const lExpected: string[] = [
-            "Dist/Test/Utils/HelperFunctions.test.js",
-            "Dist/Test/Utils/MapArgs.test.js",
-        ];
-        expect(lActual.sort()).to.eql(lExpected.sort());
-    });
+suite("GetAllTestFiles");
 
-    it("gets all files when given an empty filter", () =>
-    {
-        const lActual: string[] = GetAllTestFiles(TEST_DIR, ".$").sort();
-        const lExpected: string[] = [
-            "Dist/Test/Utils/HelperFunctions.test.d.ts",
-            "Dist/Test/Utils/HelperFunctions.test.js",
-            "Dist/Test/TestFile.demo.d.ts",
-            "Dist/Test/TestFile.demo.js",
-        ].sort();
-        expect(lActual).to.eql(lExpected);
-    });
+test("correctly filters specified files", () =>
+{
+    const lActual: string[] = GetAllTestFiles(TEST_DIR);
+    const lExpected: string[] = [
+        "Dist/Test/Utils/HelperFunctions.test.js",
+        "Dist/Test/Utils/MapArgs.test.js",
+    ];
+    expect(lActual.sort()).to.eql(lExpected.sort());
 });
 
-describe("GetMatchingFiles", () =>
+test("gets all files when given an empty filter", () =>
 {
-    it("finds demo file", () =>
-    {
-        const lActual: string[] = GetMatchingFiles(["TestFile"], "demo");
-        const lExpected: string[] = ["Dist/Test/TestFile.demo.js"];
-        expect(lActual).to.eql(lExpected);
-    });
+    const lActual: string[] = GetAllTestFiles(TEST_DIR, ".$").sort();
+    const lExpected: string[] = [
+        "Dist/Test/Utils/HelperFunctions.test.d.ts",
+        "Dist/Test/Utils/HelperFunctions.test.js",
+        "Dist/Test/TestFile.demo.d.ts",
+        "Dist/Test/TestFile.demo.js",
+    ].sort();
+    expect(lActual).to.eql(lExpected);
 });
 
-describe("MapArgs", () =>
+
+suite("GetMatchingFiles");
+
+test("finds demo file", () =>
 {
-    it("collects values from the same key", () =>
-    {
-        const lArgs: string[] = ["--files", "file1", "file2", "-p", "1", "-v"];
-        const lMap: Map<string, string[]> = new Map([
-            ["files", ["file1", "file2"]],
-            ["p", ["1"]],
-            ["v", []],
-        ]);
+    const lActual: string[] = GetMatchingFiles(["TestFile"], "demo");
+    const lExpected: string[] = ["Dist/Test/TestFile.demo.js"];
+    expect(lActual).to.eql(lExpected);
+});
 
-        const lResult: Map<string, string[]> = MapArgs(lArgs);
+suite("MapArgs");
 
-        expect(lResult).to.eql(lMap);
-    });
+test("collects values from the same key", () =>
+{
+    const lArgs: string[] = ["--files", "file1", "file2", "-p", "1", "-v"];
+    const lMap: Map<string, string[]> = new Map([
+        ["files", ["file1", "file2"]],
+        ["p", ["1"]],
+        ["v", []],
+    ]);
+
+    const lResult: Map<string, string[]> = MapArgs(lArgs);
+
+    expect(lResult).to.eql(lMap);
 });
