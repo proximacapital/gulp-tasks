90e07a2c97ac0b633e2a88b4519afae3bb6783d6
Oliver Chalk <oliverchalk245@gmail.com>
2022-01-12 20:32:46 +0100

    feat: add Gulpfile.ts to enable ci

--- Gulpfile.ts
+++ Gulpfile.ts
@@ -1,44 +1,47 @@
-import { DIST_DIR } from "@Src/Utils/Paths";
-import { CheckNode } from "@Tasks/CheckNode";
-import { ESLintCheck, ESLintFix, Lint, LintFix, MDLintCheck, MDLintFix } from "@Tasks/Lint";
-import gulp, { TaskFunctionCallback } from "gulp";
 import {
     Build,
     BuildCheckTest,
     BuildTest,
+    CheckNode,
     Clean,
     Compile,
     Copy,
     Coverage,
     Demo,
+    ESLintCheck,
+    ESLintFix,
     ExecTask,
+    Lint,
+    LintFix,
+    MDLintCheck,
+    MDLintFix,
     Test,
-} from "./Src";
+} from "@Src/index";
+import gulp, { TaskFunctionCallback } from "gulp";
 
+// ***** Base Tasks *****
+gulp.task("build", Build);
 gulp.task("check-node", CheckNode);
 gulp.task("clean", Clean);
 gulp.task("compile", Compile);
-gulp.task("copy", Copy);
-gulp.task("build", Build);
-gulp.task("eslint-check", ESLintCheck);
-gulp.task("eslint-fix", ESLintFix);
-gulp.task("mdlint-check", MDLintCheck);
-gulp.task("mdlint-fix", MDLintFix);
-gulp.task("lint-check", Lint);
-gulp.task("lint-fix", LintFix);
-gulp.task("test", Test);
-gulp.task("demo", Demo);
 gulp.task("coverage", Coverage);
+gulp.task("copy", Copy);
+gulp.task("demo", Demo);
+gulp.task("eslint", ESLintCheck);
+gulp.task("eslint-fix", ESLintFix);
+gulp.task("lint", Lint);
+gulp.task("lint-fix", LintFix);
+gulp.task("mdlint", MDLintCheck);
+gulp.task("mdlint-fix", MDLintFix);
+gulp.task("test", Test);
 
+// ***** Custom Tasks *****
 gulp.task("start", (done: TaskFunctionCallback): void =>
 {
-    ExecTask(`node ${DIST_DIR}/Src/App.js`, done);
+    ExecTask("node Dist/Src/App.js", done);
 });
 
-// ----------
-// Aliases:
+// ***** Aliases *****
 gulp.task("tests-all", gulp.task("test"));
 gulp.task("build-test", BuildTest);
-gulp.task("build-check-test", BuildCheckTest);
 gulp.task("build-lint-test", BuildCheckTest);
-
