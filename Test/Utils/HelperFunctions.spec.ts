aca8f8447662f56e1a6116a4a642b4a5122f91a3
stevenpollack <stevenpollack@users.noreply.github.com>
2022-01-13 11:11:38 +1100

    fix: make ROOT_DIR absolute path
    * refactor: make ROOT_DIR absolute path
    * fix: use absolute paths for unit tests
    * fix: pin volta npm & make sure tsc builds before mocha is run
    * ci: use npm test for testing step

--- Test/Utils/HelperFunctions.spec.ts
+++ Test/Utils/HelperFunctions.spec.ts
@@ -1,44 +1,51 @@
 import { GetAllTestFiles, GetMatchingFiles, MapArgs } from "@Src/Utils/HelperFunctions";
 import { TEST_DIR } from "@Src/Utils/Paths";
 import { expect } from "chai";
+import * as path from "path";
 
 suite("GetAllTestFiles");
 
-test("correctly filters specified files", () =>
+test("correctly filters specified files", function()
 {
     const lActual: string[] = GetAllTestFiles(TEST_DIR);
     const lExpected: string[] = [
-        "Dist/Test/Utils/HelperFunctions.test.js",
-        "Dist/Test/Utils/MapArgs.test.js",
-    ];
+        "Utils/MapArgs.test.js",
+    ]
+        .map((aPath: string) => path.join(TEST_DIR, aPath))
+        .sort();
+
     expect(lActual.sort()).to.eql(lExpected.sort());
 });
 
-test("gets all files when given an empty filter", () =>
+test("gets all files when given an empty filter", function()
 {
     const lActual: string[] = GetAllTestFiles(TEST_DIR, ".$").sort();
     const lExpected: string[] = [
-        "Dist/Test/Utils/HelperFunctions.test.d.ts",
-        "Dist/Test/Utils/HelperFunctions.test.js",
-        "Dist/Test/TestFile.demo.d.ts",
-        "Dist/Test/TestFile.demo.js",
-    ].sort();
+        "TestFile.demo.d.ts",
+        "TestFile.demo.js",
+        "Utils/HelperFunctions.spec.d.ts",
+        "Utils/HelperFunctions.spec.js",
+        "Utils/MapArgs.test.d.ts",
+        "Utils/MapArgs.test.js",
+    ]
+        .map((aPath: string) => path.join(TEST_DIR, aPath))
+        .sort();
     expect(lActual).to.eql(lExpected);
 });
 
 
 suite("GetMatchingFiles");
 
-test("finds demo file", () =>
+test("finds demo file", function()
 {
     const lActual: string[] = GetMatchingFiles(["TestFile"], "demo");
-    const lExpected: string[] = ["Dist/Test/TestFile.demo.js"];
+    const lExpected: string[] = [path.join(TEST_DIR, "TestFile.demo.js")];
     expect(lActual).to.eql(lExpected);
 });
 
 suite("MapArgs");
 
-test("collects values from the same key", () =>
+test("collects values from the same key", function()
 {
     const lArgs: string[] = ["--files", "file1", "file2", "-p", "1", "-v"];
     const lMap: Map<string, string[]> = new Map([
